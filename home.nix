e1fc3fab742be0b0a9dc7f8c5827b82c31c2e416
ingineous <ingineousus@gmail.com>
2024-11-04 02:33:12 +0530

    hyprpaper works

--- home.nix
+++ home.nix
@@ -291,6 +291,16 @@
       };
   };
 
+  services.hyprpaper = {
+	enable = true;
+	settings = {
+		ipc = "off";
+		splash = false;
+		preload = ["/home/aqua/Pictures/wallpapers/upaqua.png"];
+		wallpaper = [", /home/aqua/Pictures/wallpapers/upaqua.png"];
+	};
+  };
+
   # ------------------------------------------
   # Hyprlock
   # ------------------------------------------
