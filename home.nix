c8235c62fc6e2a25a832b7262ee3613f29ddbfc1
ingineous <ingineousus@gmail.com>
2024-11-04 02:18:02 +0530

    hyprlock and hypridle works

--- home.nix
+++ home.nix
@@ -3,6 +3,11 @@
   home.homeDirectory = "/home/aqua";
 
   home.packages = with pkgs; [
+    qt6ct
+    qt6.qtwayland
+
+    xdg-desktop-portal-hyprland
+
     # trash tool
     vscode
 
@@ -26,6 +31,8 @@
 
     btop
     htop
+
+    light
   ];
   
   programs.floorp = {
@@ -45,11 +52,12 @@
 
     settings = {
       "$mod" = "SUPER";
-      "$shiftMod" = "SUPER_SHIFT";
+      "$shiftMod" = "SUPER SHIFT";
 
       "$terminal" = "kitty";
       "$browser" = "floorp";
       "$fileManager" = "nautilus";
+      "$menu" = "wofi --show drun --allow-images";
       "$TFileManager" = "$terminal -e yazi";
 
       # ------------------------------------
@@ -70,6 +78,10 @@
 	 "ELECTRON_OZONE_PLATFORM_HINT, AUTO"
 
 	 "GTK_WAYLAND_DISABLE_WINDOWDECORATION, 1"
+
+	 "XCURSOR_SIZE, 24"
+	 "HYPRCURSOR_SIZE, 24"
+	 "QT_QPA_PLATFORMTHEME.qt6ct"
       ];
 
       # ------------------------------------------
@@ -78,6 +90,7 @@
 
       exec-once = [
          "waybar"
+		 "hypridle"
       ];
 
       # ------------------------------------------
@@ -93,14 +106,279 @@
 	  force_zero_scaling = true;
       };
 
+      windowrule = [
+        "opacity 1.0 override 0.85 override, class:zen"
+        "opacity 0.9 override 0.85 class:webstorm"
+	"opacity 1.0 override 1.0 override title:^(Spotify)$"
+	"opacity 1.0 override 0.6 override, class:vesktop"
+	"size 800 600, class:zen-browser, title:Save Image"
+	"suppressevent maximize, class:.*"
+      ];
+      
       # --------------------------------------------
       # Keybinds
       # --------------------------------------------
+
       bind = [
         "$mod, Return, exec, $terminal"
+	"$mod, W, killactive"
 	"$mod, M, exit"
+	"$mod, E, exec, $fileManager"
+	"$mod, V, togglefloating"
+	"$mod, F, exec, $menu"
+	"$mod, P, pseudo" # dwindle
+	"$mod, Space, togglesplit"
+	"$mod, B, exec, $browser"
+	"$shiftMod, right, resizeactive, 100 0"
+	"$shiftMod, left, resizeactive, -100 0"
+	"$shiftMod, down, resizeactive, 0 100"
+	"$shiftMod, up, resizeactive, 0 -100"
+	"$mod, L, exec, killall waybar || waybar"
+	"$mod, T, fullscreen, 1"
+	"$mod, Y, fullscreen, 0"
+
+	# move windows
+	"$shiftMod, A, movewindow, l"
+	"$shiftMod, D, movewindow, r"
+	"$shiftMod, W, movewindow, u"
+	"$shiftMod, S, movewindow, d"
+
+	# move focus of windows
+	"$mod, left, movefocus, l"
+	"$mod, right, movefocus, r"
+	"$mod, up, movefocus, u"
+	"$mod, down, movefocus, d"
+
+	# alt tab
+	"ALT, Tab, cyclenext"
+	"ALT, Tab, bringactivetotop"
+
+	# switch workspaces
+	"$mod, 1, workspace, 1"
+	"$mod, 2, workspace, 2"
+	"$mod, 3, workspace, 3"
+	"$mod, 4, workspace, 4"
+	"$mod, 5, workspace, 5"
+	"$mod, 6, workspace, 6"
+	"$mod, 7, workspace, 7"
+	"$mod, 8, workspace, 8"
+	"$mod, 9, workspace, 9"
+	"$mod, 0, workspace, 10"
+
+	# move active window to workspace
+	"$mod, 1, movetoworkspace, 1"
+	"$mod, 2, movetoworkspace, 2"
+	"$mod, 3, movetoworkspace, 3"
+	"$mod, 4, movetoworkspace, 4"
+	"$mod, 5, movetoworkspace, 5"
+	"$mod, 6, movetoworkspace, 6"
+	"$mod, 7, movetoworkspace, 7"
+	"$mod, 8, movetoworkspace, 8"
+	"$mod, 9, movetoworkspace, 9"
+	"$mod, 0, movetoworkspace, 0"
       ];
+
+      # move and resize windows while pressing SUPER	
+      bindm = [
+	"$mod, mouse:272, movewindow"
+	"$mod, mouse:273, resizewindow"
+      ];
+
+      general = {
+        gaps_in = 3.5;
+	gaps_out = 6;
+	border_size = 3;
+
+	"col.active_border" = "white rgba(59595900) grey 180deg";
+	"col.inactive_border" = "rgba(59595900)";
+
+	resize_on_border = true;
+	allow_tearing = true;
+
+	layout = "dwindle";
+      };
+
+      decoration = {
+         rounding = 10;
+
+	 active_opacity = 0.85;
+	 inactive_opacity = 0.6;
+	 fullscreen_opacity = 1.0;
+
+	 blur = {
+	    enabled = true;
+	    size = 10;
+	    passes = 2;
+	    new_optimizations = true;
+	    ignore_opacity = true;
+	    noise = 0.08;
+	    vibrancy_darkness = 0.1;
+	    brightness = 0.50;
+	 };
+      };
+
+      # animations
+      animations = {
+	 enabled = true;
+	 bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+	 animation = [
+	 "windows, 1, 7, myBezier"
+	 "windowsOut, 1, 7, default, popin 80%"
+	 "border, 1, 10, default"
+	 "borderangle, 1, 8, default"
+	 "fade, 1, 7, default"
+	 "workspaces, 1, 6, default"
+	 ];
+      };
+
+      dwindle = {
+	 pseudotile = true;
+	 preserve_split = true;
+      };
+
+      master = {
+        new_status = "master";
+      };
+
+      misc = {
+	force_default_wallpaper = -1;
+	disable_hyprland_logo = true;
+      };
+
+      input = {
+	kb_layout = "us";
+	kb_variant = "";
+
+	follow_mouse = 1;
+
+	sensitivity = 0;
+
+	touchpad = {
+	   natural_scroll = true;
+	};
+      };
+
+      gestures = {
+	workspace_swipe = true;
+      };
     };
+  };
+
+
+  services.hypridle = {
+      enable = true;
+
+      settings = {
+	general = {
+	   lock_cmd = "pidof hyprlock || hyprlock";
+	   before_sleep_cmd = "playerctl pause ; hyprlock";
+	   ignore_dbus_inhibit = false;
+	};
+
+	listener = [
+	   {
+	      timeout = 10;
+	      on-timeout = "loginctl lock-session";
+	   }
+
+	   {
+	      timeout = 5;
+	      on-timeout = "light -S 5";
+	      on-resume = "light -S 20";
+	   }
+	];
+      };
+  };
+
+  # ------------------------------------------
+  # Hyprlock
+  # ------------------------------------------
+  programs.hyprlock = {
+     enable = true;
+     
+	 settings = {
+		 background = [
+     {
+        monitor = "";
+	path = "~/Pictures/wallpapers/marima.jpg";
+	color = "rgba(25, 20, 20, 1.0)";
+
+	blur_passes = 0;
+	blur_size = 2;
+	noise = 0;
+	contrast = 0;
+	brightness = 0;
+	vibrancy = 0;
+	vibrancy_darkness = "0.0";
+     }
+     ];
+
+     input-field = [
+	{
+	   monitor = "";
+	   size = "400, 80";
+	   outline_thickness = 2;
+	   dots_size = 0.25;
+	   dots_spacing = 0.55;
+	   dots_center = true;
+	   dots_rounding = -1;
+	   outer_color = "rgb(236, 169, 158)";
+	   inner_color = "rgb(135, 27, 43)";
+	   font_color = "rgb(179, 206, 208)";
+	   fade_on_empty = false;
+	   placeholder_text = "";
+	   hide_input = false;
+	   check_color = "rgba(204, 136, 34, 0)";
+	   fail_color = "rgba(204, 34, 34, 0)";
+	   fail_text = "$FAIL <b>($ATTEMPTS)</b>";
+	   fail_transition = 300;
+	   capslock_color = -1;
+	   numlock_color = -1;
+	   bothlock_color = -1;
+	   invert_numlock = false;
+	   swap_font_color = false;
+	   position = "0, -270";
+	   halign = "center";
+	   valign = "center";
+	}
+      ];
+
+      label = [
+        {
+	   monitor = "";
+	   text = "cmd[update:1000] echo '$(data + '%A, %B %d')'";
+	   color = "rgba(215, 130, 147, 0.75)";
+	   font_size = 80;
+	   font_family = "SF Pro Display Bold";
+	   position = "0, 400";
+	   halign = "center";
+	   valign = "center";
+	 }
+
+	 {
+	   monitor = "";
+	   text = "cmd[update:1000] echo '$(date +'%k:%M')'";
+	   color = "rgba(179, 206, 208, 0.75)";
+	   font_size = 140;
+	   font_family = "SF Pro Display Bold";
+	   position = "0, 253";
+	   halign = "center";
+	   valign = "center";
+	 }
+
+	 {
+	   monitor = "";
+	   text = "Touch Id or Enter Password";
+	   color = "rgba(242, 243, 244, 0.75)";
+	   font_size = 10;
+	   font_family = "SD Pro Display";
+	   position = "0, -242";
+	   halign = "center";
+	   valign = "center";
+	 }
+     ];
+	 };
   };
 
   programs.home-manager.enable = true;
