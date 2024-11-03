d8e9f014648d992d72978e05c48063a4bbf790bd
ingineous <ingineousus@gmail.com>
2024-11-03 12:04:34 +0530

    home manager works

--- flake.nix
+++ flake.nix
@@ -4,18 +4,26 @@
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.hoshino = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+    nixosConfigurations = {
+      hoshino = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.aqua = import ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
