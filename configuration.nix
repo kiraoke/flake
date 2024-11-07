ec71b267417e8eb697d5df783c909988b9d324b0
ingineous <ingineousus@gmail.com>
2024-11-08 04:29:55 +0530

    nvidia prime offload

--- configuration.nix
+++ configuration.nix
@@ -106,10 +106,20 @@
   services.xserver.videoDrivers = ["nvidia" "amdgpu"];
   hardware.nvidia = {
      modesetting.enable = true;
+
+     prime = {
+       offload = {
+         enable = true;
+	 enableOffloadCmd = true;
+       };
+
+       amdgpuBusId = "PCI:5:0:0";
+       nvidiaBusId = "PCI:1:0:0";
+     };
      
      # see arch wiki preserve nvidia video memory
      powerManagement.enable = true;
-#     powerManagement.finegrained = true; requires prime offload
+     powerManagement.finegrained = true;
 
      open = false; # disable the new nvidia open source drivers
 
