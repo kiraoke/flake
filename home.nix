d43a3d759d62339201f6c8fc6c2e96bd39eb310f
ingineous <ingineousus@gmail.com>
2024-11-08 04:36:18 +0530

    some tweaks

--- home.nix
+++ home.nix
@@ -502,8 +502,8 @@
     };
 
     extraConfig = ''
-	monitor= eDP-1 ,2560x1600@165,auto,1.33
-	monitor = , preferred, auto, 1, mirror, eDP-1
+	monitor= eDP-2 ,2560x1600@165,auto,1.33
+	monitor = , preferred, auto, 1, mirror, eDP-2
 
 	windowrulev2 = opacity 1.0 override 0.6 override, class:zen
 	windowrulev2 = opacity 0.9 override 0.85 override, class:webstorm
