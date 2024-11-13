41b1a6ff90f289abe9d168938f3ff31e5631d530
kiraoke <kiraokeku@gmail.com>
2024-11-13 12:25:07 +0530

    some neovim shit

--- home.nix
+++ home.nix
@@ -12,6 +12,9 @@
     cloak.enable = true;
 
     harpoon.enable = true;
+    trouble.enable = true;
+    "zen-mode".enable = true;
+    fugitive.enable = true;
 	};
 
 	extraConfigLua = ''
@@ -87,7 +90,53 @@ vim.opt.colorcolumn = "80"
 	vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end) 
 	vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
  
+   -- trouble settings
+        local trouble = require("trouble")
+        trouble.setup({
+            icons = false,
+        })
 
+        vim.keymap.set("n", "<leader>tt", trouble.toggle)
+        vim.keymap.set("n", "<leader>tn", function() trouble.next({skip_groups=true, jump=true}) end)
+        vim.keymap.set("n", "<leader>tp", function() trouble.previous({skip_groups=true, jump=true}) end)
+  -- zen mode settings
+
+
+  -- fugitive settings
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+        local Ingineous_Fugitive = vim.api.nvim_create_augroup("Ingineous_Fugitive", {})
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = Ingineous_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = {buffer = bufnr, remap = false}
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git('push')
+                end, opts)
+
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({'pull',  '--rebase'})
+                end, opts)
+
+                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+                -- needed if i did not set the branch up correctly
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+            end,
+        })
+
+
+        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+ 
 	'';
     };
 
