7efc269d90f95860559d352169c7fd2faaa49ee0
kiraoke <kiraokeku@gmail.com>
2024-12-06 09:22:52 +0530

    before

--- system/xserver.nix
+++ system/xserver.nix
@@ -2,17 +2,17 @@
   services.xserver = {
     enable = true;
     xkb.layout = "us";
-    desktopManager.plasma5.enable = true;
+#    desktopManager.plasma5.enable = true;
   };
 
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    plasma-browser-integration
-    konsole
-    oxygen
-    ark
-    elisa
-    gwenview
-    spectacle
-    okular
-  ];
+# environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+#   plasma-browser-integration
+#   konsole
+#   oxygen
+#   ark
+#   elisa
+#   gwenview
+#   spectacle
+#   okular
+# ];
 }
