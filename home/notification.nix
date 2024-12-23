4b69357dcc69905cd032f8e1b5c715d52a32d00b
kiraoke <kiraokeku@gmail.com>
2024-12-24 01:42:24 +0530

    update

--- home/notification.nix
+++ home/notification.nix
@@ -6,11 +6,10 @@
       .notification {
         padding: 10px;
         border-radius: 10px;
-        background: rgba(32, 23, 23, 0.85);
+        background: rgba(32, 23, 23, 0.75);
         border: 4px solid #8d7091;
         color: #d7d7d5;
       }
-
     '';
   };
 }
