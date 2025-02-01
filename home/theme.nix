63c006a68affec093ddde2d2396cf11f6c1fbcda
kiraoke <kiraokeku@gmail.com>
2025-02-02 03:55:20 +0530

    rose pine sucks

--- home/theme.nix
+++ home/theme.nix
@@ -20,16 +20,8 @@
     };
 
     theme = {
-      name = "Rose-Pine-Dark";
+      name = "rose-pine";
       package = pkgs.rose-pine-gtk-theme;
-    };
-
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
-
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
     };
   };
 
