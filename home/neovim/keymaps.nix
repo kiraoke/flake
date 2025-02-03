b876e819ba18e2bc999843622f209c71212bb077
kiraoke <kiraokeku@gmail.com>
2025-02-03 05:39:35 +0530

    code actions

--- home/neovim/keymaps.nix
+++ home/neovim/keymaps.nix
@@ -25,6 +25,13 @@
       options = {
         desc = "Format the code using lsp";
       };
+    }{
+      mode = "n";
+      key = "<leader>ca";
+      action.__raw = "vim.lsp.buf.code_action";
+      options = {
+        desc = "Get code actions";
+      };
     }
     {
       mode = "n";
