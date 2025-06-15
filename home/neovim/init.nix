5ba2c59ba4416d3068cd80f3e2eda4472c291f23
kiraoke <kiraokeku@gmail.com>
2025-06-16 00:22:35 +0530

    smthin

--- home/neovim/init.nix
+++ home/neovim/init.nix
@@ -1,19 +1,10 @@
-{...}: {
+{ ...}: {
   programs.nixvim.enable = true;
 
   programs.nixvim.extraConfigLuaPost = ''
     local nvim_lsp = require('lspconfig')
 
-    nvim_lsp.denols.setup ({
-        on_attach = on_attach,
-        root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
-        })
 
-    nvim_lsp.ts_ls.setup ({
-      on_attach = on_attach,
-      root_dir = nvim_lsp.util.root_pattern("package.json"),
-      single_file_support = false
-      })
   '';
 
   imports = [
@@ -30,3 +21,13 @@
     ./debugger.nix
   ];
 }
+# nvim_lsp.denols.setup ({
+#     on_attach = on_attach,
+#     root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+#     })
+#
+# nvim_lsp.ts_ls.setup ({
+#     on_attach = on_attach,
+#     root_dir = nvim_lsp.util.root_pattern("package.json"),
+#     single_file_support = false
+#     })
