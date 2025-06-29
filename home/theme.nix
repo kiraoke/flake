03c9e59ab8a508877fc3e6b6cc8036ff86885d6c
kiraoke <kiraokeku@gmail.com>
2025-06-29 18:00:00 +0530

    sugoi desu

--- home/theme.nix
+++ home/theme.nix
@@ -4,9 +4,8 @@
     hyprlock.enable = false;
     neovim.enable = false;
     nixvim.enable = false;
-    fzf.enable = false;
-    bat.enable = false;
     kitty.enable = false;
+    fzf.enable = false;
   };
 
   home.file.".local/share/fonts/DIN" = {
