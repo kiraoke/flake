5627228fa2e07d28128b6129beacbe62a2cec9a9
kiraoke <kiraokeku@gmail.com>
2025-10-15 19:54:19 +0530

    remove ts ls because deno ls fucking it

--- home/neovim/init.nix
+++ home/neovim/init.nix
@@ -2,20 +2,18 @@
   programs.nixvim.enable = true;
 
   programs.nixvim.extraConfigLuaPost = ''
-      vim.g.rust_recommended_style = 0
+    vim.g.rust_recommended_style = 0
 
-      vim.lsp.config('denols', {
-          cmd = { 'deno', 'lsp' },
-          root_dir = vim.fs.root(0, { 'deno.json', 'deno.jsonc' }),
+    vim.lsp.config('denols', {
           on_attach = on_attach,
-      })
+          root_markers = {"deno.json", "deno.jsonc"},
+          })
 
-      vim.lsp.config('ts_ls', {
-          cmd = { 'typescript-language-server', '--stdio' },
-          root_dir = vim.fs.root(0, { 'package.json' }),
-          single_file_support = false,
-          on_attach = on_attach,
-       })
+    vim.lsp.config('ts_ls', {
+        on_attach = on_attach,
+        root_markers = {"package.json"},
+        single_file_support = false,
+        })
   '';
 
   imports = [
