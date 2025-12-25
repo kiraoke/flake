762ad51b529c0e6b975105a4ea32219333cc6b4d
zenzawa <kiraokeku@gmail.com>
2025-12-25 12:14:34 +0530

    60

--- home/hyprland/extraConfig.nix
+++ home/hyprland/extraConfig.nix
@@ -1,8 +1,8 @@
 {...}
 : let
   config = ''
-    monitor= eDP-1 ,2560x1600@165,auto,1.33
-    monitor= eDP-2 ,2560x1600@165,auto,1.33
+    monitor= eDP-1 ,2560x1600@60,auto,1.33
+    monitor= eDP-2 ,2560x1600@60,auto,1.33
     monitor = , preferred, auto, 1, mirror, eDP-2
 
     windowrulev2 = opacity 1.0 override 0.85 override, class:zen-beta
